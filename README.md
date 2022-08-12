4ef5b45a2a01de875fedc2388c2304ab99368eff
Alice <snow.alice@pm.me>
2022-08-12 15:22:03 +0200

    push first ruch

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# oidc
+# OIDC
+This module is used to manage user login with OIDC & [Authentik](https://goauthentik.io/)
+
+> tested on version: 2022.8
+
+# How it's work
+
+## Step 1: init the module
+Init the module with required var: (endpoint, client_id, client_secret, redirectUrl)
+
+| Name | Type | Description |
+| :--- | :--- | :---------- |
+| endpoint | string | the base endpoint, for example `https://auth.alice-snow.me` |
+| client_id | string | your app client id |
+| client_secret | string | your app client secret |
+| redirectUrl | string |  the callback uri, for example `https://myFamulousProject.amelia.lu/login/callback` |
+
+```ts
+import { OICD } from "https://deno.land/x/oicd/mod.ts";
+const myoidc = new OICD("https://auth.alice-snow.me", MY_CLIENT_PUB, MY_CLIENT_PRIV, "https://myFamulousProject.amelia.lu/login/callback")
+```
+
+## Step 2: (facultative) get your redirection url
+You need: (scope, states)
+
+| Name | Type | Description |
+| :--- | :--- | :---------- |
+| scope | string | the auth scope for the user, for example `["openid", "profile", "email"]` |
+| states | string | random string block security issue (keep blank if you want) |
+
+```ts
+const redirectUrl = myoidc.generateAuthUrl(["openid", "profile", "email"], "aliceIsAPrincess") // "aliceIsAPrincess" is actualy a random string :D
+```
+
+> redirect the user to this url to perform the authentication
+
+## Step 3: get the user infos
+App is theoretically responding you after the authentication.
+
+Format: `/login/callback?code=HERE_THE_CODE&state=aliceIsAPrincess`
+
+call `OIDC.authUser(code)` to get back the data.
+
+```ts
+const userData = myoidc.authUser(HERE_THE_CODE)
+
+/* 
+Successfull response: 
+{
+    sucess: true,
+    data: USER_DATA
+};
+
+USER_DATA example data: 
+{
+    "email": "snow.alice@pm.me",
+    "email_verified": true,
+    "name": "Alice",
+    "given_name": "Alice",
+    "family_name": "Snow",
+    "preferred_username": "alice",
+    "nickname": "alice",
+    "groups": [
+        "user"
+    ],
+    "sub": "buyvaAB86B9A87A098908978VVGHVJHhvad"
+}
+*/
+
+```
---
+++ mod.ts
@@ -0,0 +1,75 @@
+export class OICD {
+    private endpoint: string;
+    private client_id: string;
+    private client_secret: string;
+    private redirectUrl: string;
+
+    constructor(endpoint: string, client_id: string, client_secret: string, redirectUrl: string) {
+        this.endpoint = endpoint;
+        this.client_id = client_id;
+        this.client_secret = client_secret;
+        this.redirectUrl = redirectUrl;
+    }
+
+
+    // generate the authorization url
+    public generateAuthUrl(scopes:string[], state:string) {
+        return `${this.endpoint}/authorize/?client_id=${this.client_id}&redirect_uri=${this.redirectUrl}&response_type=code&scope=${scopes.join(" ")}&state=${state}`;
+    }
+
+    public async authUser(code:string) {
+        const req = await fetch(`${this.endpoint}/application/o/token/`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Auhorization": `Basic ${btoa(`${this.client_id}:${this.client_secret}`)}`
+            },
+            body: `grant_type=authorization_code&code=${code}&redirect_uri=${this.redirectUrl}`
+        })
+
+        try {
+            const response = await req.json();
+            if(response.access_token) {
+                return await this.getUserData(response.access_token);
+            } else {
+                return {
+                    success: false,
+                    error: "Invalid response, no access token"
+                }
+            }
+        } catch (error) {
+            return {
+                success: false,
+                error: "Invalid response"
+            }
+        }
+
+    }
+
+
+
+    private async getUserData(access_token){
+        const req = await fetch(`${this.endpoint}/application/o/userinfo/`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Auhorization": `Basic ${btoa(`${this.client_id}:${this.client_secret}`)}`
+            },
+            body: `access_token=${access_token}`
+        })
+
+        try {
+            const response = await req.json();
+            return {
+                sucess: true,
+                data: response
+            };
+        } catch (error) {
+            return {
+                success: false,
+                error: "Invalid response, cannot get user data"
+            }
+        }
+    }
+
+}
